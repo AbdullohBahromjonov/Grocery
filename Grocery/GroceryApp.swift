ed9ff222427df01727de5f9c77afdf764b7db58d
Abdulloh <abdullohbahromjonv@gmail.com>
2024-02-05 21:42:45 +0500

    Initial Commit

---
+++ Grocery/GroceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryApp.swift
+//  Grocery
+//
+//  Created by Abdulloh Bahromjonov on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
