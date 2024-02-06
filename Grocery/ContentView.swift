0b4d2a4c8bf4eed5b57062c40fa041d73f6d20d7
Abdulloh <abdullohbahromjonv@gmail.com>
2024-02-06 06:23:13 +0500

    Added nav bar items, created 'Цена по карте' button

--- Grocery/ContentView.swift
+++ Grocery/ContentView.swift
@@ -9,13 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView(.vertical) {
+                VStack {
+                    Button(
+                        action: {},
+                        label: {
+                            Text("Цена по карте")
+                                .font(.caption2)
+                                .padding(.horizontal, 5)
+                                .padding(.vertical, 5)
+                                .foregroundColor(.white)
+                                .background(
+                                    Color.green
+                                )
+                                .cornerRadius(4)
+                        }
+                    )
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
+            }
+            .padding(.horizontal)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(
+                        action: {},
+                        label: {
+                            Image(systemName: "arrow.left")
+                                .foregroundColor(.green)
+                                .font(.system(size: 18, weight: .semibold))
+                        }
+                    )
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 5) {
+                        Button(
+                            action: {},
+                            label: {
+                                Image(systemName: "doc.plaintext")
+                                    .foregroundColor(.green)
+                                    .font(.system(size: 16, weight: .semibold))
+                            }
+                        )
+                        Button(
+                            action: {},
+                            label: {
+                                Image(systemName: "square.and.arrow.up")
+                                    .foregroundColor(.green)
+                                    .font(.system(size: 16, weight: .semibold))
+                            }
+                        )
+                        Button(
+                            action: {},
+                            label: {
+                                Image(systemName: "heart")
+                                    .foregroundColor(.green)
+                                    .font(.system(size: 16, weight: .semibold))
+                            }
+                        )
+                    }
+                }
+                
+                
+            }
         }
-        .padding()
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
